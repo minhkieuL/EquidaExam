ddba3ee9787f2d6d61a1fa43004230535d3d6c91
MartinJ <martinj50220@gmail.com>
2018-10-12 09:12:41 +0200

    Implementation de la classe modele Participer

--- src/src/java/modele/Participer.java
+++ src/src/java/modele/Participer.java
@@ -1,5 +1,41 @@
 package modele;
 
 public class Participer {
+    private int place;
+    private Course course;
+    private Cheval cheval;
     
+    public Participer() {
+        this(0, null, null);
+    }
+
+    public Participer(int place, Course course, Cheval cheval) {
+        this.place = place;
+        this.course = course;
+        this.cheval = cheval;
+    }
+    
+    public int getPlace() {
+        return place;
+    }
+
+    public void setPlace(int place) {
+        this.place = place;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Cheval getCheval() {
+        return cheval;
+    }
+
+    public void setCheval(Cheval cheval) {
+        this.cheval = cheval;
+    }
 }
